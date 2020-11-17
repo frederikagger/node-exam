455bd66318a8d259ccaa49ddec537dbeb59d2f4a
frederikagger <frederikagger@hotmail.com>
2020-11-17 21:23:07 +0100

    initial commit

---
+++ routes/api/index.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+router.use(require("./login"));
+router.use(require("./register"));
+router.use(require("./user"));
+router.use(require("./contact"));
+
+// TODO: router virker nu, men er ikke sikker på det er en optimal løsning
+
+module.exports = router;
