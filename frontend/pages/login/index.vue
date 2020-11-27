59a901a35f1d85f58a90d198b496f9319eb3abd8
frederikagger <frederikagger@hotmail.com>
2020-11-27 23:21:22 +0100

    more styling added

--- frontend/pages/login/index.vue
+++ frontend/pages/login/index.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="container mx-auto pt-32">
+  <div class="container mx-auto pt-32" >
     <div
       id="card"
-      class="max-w-xs p-5 mx-auto flex flex-col bg-white rounded-xl h-80 lg:h-96 shadow-2x md:p-10 md:max-w-md lg:max-w-lg"
+      class="max-w-xs p-5 mx-auto flex flex-col bg-white rounded-xl h-80 lg:h-96 shadow-2x md:p-10 md:max-w-md"
     >
       <h4 class="text-4xl mb-6 font-bold text-center text-indigo-500">
         Login
@@ -10,14 +10,14 @@
       <input
         v-model="email"
         id="email"
-        class="p-1 placeholder-gray-700 focus:placeholder-gray-400 border mb-4 text-black text-lg bg-indigo-50 rounded-sm border-transparent focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
+        class="p-1 placeholder-gray-600 focus:placeholder-gray-400 border mb-4 text-black text-lg bg-indigo-50 rounded-md border-transparent bg-indigo-100 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
         type="text"
         placeholder="Email"
       />
       <input
         v-model="password"
         id="password"
-        class="p-1 placeholder-gray-700 focus:placeholder-gray-400 border mb-4 text-black text-lg bg-indigo-50 rounded-sm border-transparent focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
+        class="p-1 placeholder-gray-600 focus:placeholder-gray-400 border mb-4 text-black text-lg bg-indigo-50 rounded-md border-transparent bg-indigo-100 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
         type="password"
         placeholder="Password"
       />
@@ -59,4 +59,5 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+</style>
