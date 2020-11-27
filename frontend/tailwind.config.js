be3efbeb25427ef96aeb11caba81747a91e7e5d9
frederikagger <frederikagger@hotmail.com>
2020-11-27 23:20:45 +0100

    tailwindconfig file created

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: {
+    enabled: process.env.NODE_ENV === "production",
+    content: [
+      "components/**/*.vue",
+      "layouts/**/*.vue",
+      "pages/**/*.vue",
+      "plugins/**/*.js",
+      "nuxt.config.js",
+    ]
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {}
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+};
