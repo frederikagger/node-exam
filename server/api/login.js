9587264c838fbc1cbf775723d97906d080e91f12
frederikagger <frederikagger@hotmail.com>
2020-11-28 12:50:01 +0100

    cleaned up route

--- server/api/login.js
+++ server/api/login.js
@@ -5,13 +5,12 @@ const createError = require("http-errors");
 const auth = require("../middleware/auth");
 
 router.post("/login", async (req, res, next) => {
-  const { username, password } = req.body;
   try {
-    const user = await User.findOne({username});
+    const user = await User.findOne({email: req.body.user.email});
     if (!user) {
       throw createError(400, "No user with the current username exist");
     }
-    if (await bcrypt.compare(password, user.password)) {
+    if (await bcrypt.compare(req.body.user.password, user.password)) {
       const token = await user.createJWT();
       res.status(200).send(token);
     } else throw createError(400, "The password is incorrect");
@@ -21,9 +20,8 @@ router.post("/login", async (req, res, next) => {
 });
 
 router.delete("/logout", auth, async (req, res, next) => {
-  let user = req.user;
+  const user = req.user;
   user.tokens = [];
-  user = new User(user);
   try {
     await user.save();
     return res.status(200).send("User was logged out");
