64d8975ab8b3377e1276764799d2e6d49c885511
frederikagger <frederikagger@hotmail.com>
2020-12-02 20:37:01 +0100

    fix: returning status code 400 when recieving a bad request

--- api/register.js
+++ api/register.js
@@ -1,8 +1,13 @@
 const router = require("express").Router();
 const User = require("../models/user");
+const createError = require("http-errors");
 
 router.post("/register", async (req, res, next) => {
   try{
+    const { firstname, lastname, email, password, birthday, country } = req.body.user;
+    if (!firstname, !lastname, !email, !birthday, !password, !country){
+      throw new createError(400, "Bad request");
+    }
     const user = new User(req.body.user);
     user.email = user.email.toLowerCase();
     await user.save();
