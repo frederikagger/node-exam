c373033288d0e23d33dfca8fb86679bc002e4ac6
frederikagger <frederikagger@hotmail.com>
2020-12-04 15:21:21 +0100

    fix and feat: error handling implemented and new route to insert dummy data

--- api/users.js
+++ api/users.js
@@ -2,9 +2,24 @@ const router = require("express").Router();
 const auth = require("../middleware/auth");
 const User = require("../models/user");
 
-router.get("/users", auth, async (req, res) => {
-  users = await User.find({}, "firstname lastname").exec();
-  return res.send(users);
+router.get("/users", auth, async (req, res, next) => {
+  try {
+    users = await User.find({}, "firstname lastname profilePicURL _id").exec();
+    return res.send(users);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/users", async (req, res, next) => {
+  try {
+    const { users } = req.body;
+    console.log(users);
+    users.map((user) => new User(user).save());
+    return res.status(201).send();
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
