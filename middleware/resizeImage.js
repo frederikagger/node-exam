1051978a1623799e2386bb3a2d4a72ff54da0bd8
frederikagger <frederikagger@hotmail.com>
2020-12-07 21:25:46 +0100

    comment added

--- middleware/resizeImage.js
+++ middleware/resizeImage.js
@@ -6,7 +6,7 @@ const resizeImage = async (req, res, next) => {
       .resize({ width: 150, height: 150 })
       .jpeg({ quality: 70 })
       .toBuffer();
-    req.image = buffer;
+    req.image = buffer; // attaching the resizedimage buffer to the request
     next();
   } catch (error) {
     next(error);
