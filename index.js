d56b48da16effdb7ffd185a37d09daa0f13b8cba
frederikagger <frederikagger@hotmail.com>
2020-12-08 17:23:51 +0100

    feat: socket.io added

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-const app = require("./app")
+const app = require("./app");
+const server = require("http").createServer(app);
+const io = require("./io")(server)
+
+
+io.use((socket, next) => {
+  console.log(socket.id);
+  next();
+});
+
+io.on("connection", async (socket) => {
+  console.log("client connected");
+  socket.on("chat message", ({ message }) => {
+    console.log(message);
+    io.emit("server emit", { message });
+  });
+});
 
 const port = process.env.PORT || 8000;
 
-app.listen(port, () =>
-  console.log(`app listening on port ${port}!`)
-);
+server.listen(port);
