fe95c72eefa3bb7b81be25669b0207871bf0d497
frederikagger <frederikagger@hotmail.com>
2020-12-09 08:21:05 +0100

    cors origin set in .env

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require("./app");
 const server = require("http").createServer(app);
 const io = require("socket.io")(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: process.env.ORIGIN,
     methods: ["GET", "POST"],
   },
 });
